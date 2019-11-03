e2eb85b96cd4c61786f6af06929ec4ac3e5dd6f6
Lúcio Reis <lucio@midiacom.uff.br>
2019-11-03 13:48:11 -0300

    Quality Game DJANGO REST API Models implemented

--- qualityGameRestAPI/main/admin.py
+++ qualityGameRestAPI/main/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Card
+from .models import Card, GameState, User, Event
 # Register your models here.
 
+admin.site.register(GameState)
+admin.site.register(User)
 admin.site.register(Card)
+admin.site.register(Event)
--- qualityGameRestAPI/main/models.py
+++ qualityGameRestAPI/main/models.py
@@ -1,8 +1,28 @@
 from django.db import models
 
 # Create your models here.
+# Cada usuario terá um estado de jogo que será atualizado a cada nível concluído
+class GameState(models.Model):
 
+    class Meta:
+        db_table = 'gamestate'
 
+    current_cash = models.FloatField()
+    current_approval = models.IntegerField()
+    current_client_satisfaction = models.IntegerField()
+    current_employees_satisfaction = models.IntegerField()
+    current_overrall_satisfaction = models.IntegerField()
+    current_level = models.IntegerField()
+
+class User(models.Model):
+
+    class Meta:
+        db_table = 'user'
+    user_name = models.CharField(max_length=200)
+    user_current_state = models.ForeignKey(GameState, on_delete=models.CASCADE)
+    user_profile_picture_path = models.CharField(max_length=200, blank=True)
+
+# Cada carta contem um nível ee sua descrição e opções
 class Card(models.Model):
 
     class Meta:
@@ -18,3 +38,14 @@ class Card(models.Model):
 
     def __str__(self):
         return self.card_title
+
+class Event(models.Model):
+
+    class Meta:
+        db_table = 'event'
+
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    card_cash_ct_effect = models.IntegerField()
+    card_cash_wr_effect = models.IntegerField()
+    card_approval_ct_effect = models.IntegerField()
+    card_approval_wr_effect = models.IntegerField()

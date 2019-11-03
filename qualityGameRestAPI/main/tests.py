4506e5bfc15695067ed43684bac37a902e2016fe
Lúcio Reis <lucio@midiacom.uff.br>
2019-11-03 14:17:28 -0300

    Quality Game DJANGO REST API testcase for gamestate, VERSION 0.3

--- qualityGameRestAPI/main/tests.py
+++ qualityGameRestAPI/main/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Card, User, Event, GameState
 
-# Create your tests here.
+class overrallTestCase(TestCase):
+    def setUp(self):
+        gamestate = GameState()
+        gamestate.current_cash = 2500.0
+        gamestate.current_level = 1
+        gamestate.current_approval = 100
+        gamestate.current_client_satisfaction = 100
+        gamestate.current_employees_satisfaction = 100
+        gamestate.current_overrall_satisfaction =  100
+        gamestate.save()
+
+        card = Card()
+        user = User()
+        event = Event()
+
+    def test_user_initiates_level_1(self):
+        """Check if new Users start level 1"""
